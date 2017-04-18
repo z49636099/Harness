d2f37d8de581ba0c4d7f2ed58609a4096ba4a04e
Kenyang <kenyang@atop.com.tw>
2017-04-18 17:36:06 +0800

    0418 討論

--- HarnessControl/HarnessControl/atopPortocolBase.cs
+++ HarnessControl/HarnessControl/atopPortocolBase.cs
@@ -6,10 +6,37 @@ using System.Text;
 namespace HarnessControl
 {
     public abstract class atopPortocolBase
-    {
-        public HarnessTCPClient Client { get; set; }
-        
+    {        
+        public HarnessSession Session { get; set; }
 
+        public event Action<string> StatusEvent;
+
+        public void Reliability ()
+        {
+            /* 1 day */
+            PollSataic();
+            /* 3 day */
+            PollControl();
+            /* 5 day */
+            PollChange();
+        }
+
+        public abstract void Patten(string PattenPath);
+
+        public abstract void PollControl();
+        public abstract void PollChange();
+        public abstract void PollSataic();
+
+
+        public void SendStatus(string StatusMessage)
+        {
+            StatusEvent?.Invoke(StatusMessage);
+        }
+    }
+
+
+    public abstract class DNP3:atopPortocolBase
+    {
 
     }
 }
