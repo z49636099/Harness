a4ae200ec42e197129f0949b12eedb58cdac6ee3
稟紘 黃 <just_love_angel@hotmail.com>
2017-05-04 13:07:47 +0800

    Add Protocol 1.101 2.104 3.61850

---
+++ HarnessControl/HarnessControl/Protocol/101.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HarnessControl.Protocol
+{
+    public class _101 : atopPortocolBase
+    {
+        public override void Patten(string PattenPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void PollSataic()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void PollChange()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public override void PollControl()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ HarnessControl/HarnessControl/Protocol/61850.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HarnessControl.Protocol
+{
+    public class _61850 : atopPortocolBase
+    {
+    }
+}
